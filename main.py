d35c93a1ffbed795afd810388af835f65dbfc6ea
giuseppefrusso <giuseppe.f.russo@gmail.com>
2022-06-09 19:11:20 +0200

    ENTERING INSIDE FUNCTION

--- main.py
+++ main.py
@@ -774,6 +774,9 @@ def agent_entering_management(current_agents,last_agents, entering,vehicles_dict
         if not check_entering_condition:
             del entering[id]
 
+    print("ENTERING INSIDE FUNCTION")
+    print(entering)
+
     return agents_to_consider, entering, vehicles_dict
 
 def agents_outgoing_managements(current_agents,last_agents, outgoing, vehicle_dict=None):
